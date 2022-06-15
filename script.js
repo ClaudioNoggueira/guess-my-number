19be561e861f4b61da885e7b00fceaa9c5f0a593
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-06-15 13:18:10 -0300

    Refactor(script): Use jQuery

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 // console.log(`Secret number: ${secretNumber}`);
 
@@ -6,66 +7,66 @@ let highestScore = Number(window.localStorage.getItem("highestScore")) || 1;
 
 let finished = false;
 
+let messageEl = $(`#message`);
+
 function checkAnswer(guess) {
-  const messageEl = document.getElementById(`message`);
   if (finished === false) {
     !guess
-      ? noAnswer(messageEl)
+      ? noAnswer()
       : guess === secretNumber
-      ? rightAnswer(messageEl)
-      : wrongAnswer(guess, messageEl);
+      ? rightAnswer()
+      : wrongAnswer(guess);
   }
 }
 
-function noAnswer(messageEl) {
-  messageEl.textContent = "⛔ No number!";
+function noAnswer() {
+  messageEl.text("⛔ No number!");
 }
 
-function rightAnswer(messageEl) {
+function rightAnswer() {
   finished = true;
 
-  messageEl.textContent = "🎉 You got it right!!!";
-  messageEl.style.color = "white";
-  document.body.style.backgroundColor = `green`;
+  messageEl.text("🎉 You got it right!!!");
+  messageEl.css({ color: `white` });
+  $(`body`).css({ backgroundColor: `green` });
 
-  document.getElementById(`secretNumber`).textContent = secretNumber;
+  $(`#secretNumber`).text(secretNumber);
 
   if (score > highestScore) setHighestScore(score);
 }
 
-function wrongAnswer(guess, messageEl) {
+function wrongAnswer(guess) {
   score--;
 
-  messageEl.style.color = `red`;
-  messageEl.textContent = guess < secretNumber ? `Too LOW.` : `Too HIGH.`;
+  messageEl.css({ color: `red` });
+  messageEl.text(guess < secretNumber ? `Too LOW.` : `Too HIGH.`);
 
-  document.getElementById(`score`).textContent = score;
+  $(`#score`).text(score);
 
   if (score === 0) looseGame();
 }
 
 function looseGame() {
   finished = true;
-  document.body.style.backgroundColor = `red`;
-  messageEl.style.color = `white`;
-  messageEl.textContent = "💥 You lost.";
+  messageEl.text("💥 You lost.");
+  messageEl.css({ color: `white` });
+  $(`body`).css({ backgroundColor: `red` });
 }
 
 function setHighestScore(score) {
   highestScore = score;
-  document.getElementById(`highestScore`).textContent = highestScore;
+  $(`#highestScore`).text(highestScore);
   window.localStorage.setItem(`highestScore`, highestScore);
 }
 
-document.getElementById(`score`).textContent = score;
-document.getElementById(`highestScore`).textContent = highestScore;
+$(`#score`).text(score);
+$(`#highestScore`).text(highestScore);
 
-document.getElementById(`checkButton`).addEventListener("click", () => {
-  const numberEl = document.getElementById(`number`);
-  const guess = Number(numberEl.value);
+$(`#checkButton`).click((e) => {
+  const guess = Number($(`#number`).val());
   checkAnswer(guess);
 });
 
-document.getElementById(`reloadButton`).addEventListener(`click`, () => {
+$(`#reloadButton`).click((e) => {
   location.reload();
 });
